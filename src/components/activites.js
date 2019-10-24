0ef023a08bc7a727d6abff4ddce61d671d29a086
Marc Ott <ott11marc@gmail.com>
2019-10-24 16:43:30 +0200

    Add activites

--- src/components/activites.js
+++ src/components/activites.js
@@ -6,6 +6,12 @@ import "./activites.scss";
 const Activites = () => (
 	<section className="activites">
 		<h2>Nos activités</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sem mi, finibus at orci sit amet, aliquam laoreet mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis sodales felis eu orci mattis, et volutpat nulla pellentesque.</p>
+		<div className="list-buttons">
+			<button>Peinture</button>
+			<button>Isolation</button>
+			<button>Ravallement</button>
+		</div>
 	</section>
 	
 	
